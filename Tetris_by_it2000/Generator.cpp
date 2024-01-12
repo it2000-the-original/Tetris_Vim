92ff623e2bdaae5aa59c2ce12ef0d6979075d5d0
it2000-the-original <scaranoalessandro2005@gmail.com>
2024-01-12 17:29:54 +0100

    Fixed generator.cpp for Linux

--- Tetris_by_it2000/Generator.cpp
+++ Tetris_by_it2000/Generator.cpp
@@ -1,4 +1,5 @@
 #include "Generator.hpp"
+#include <cstdlib>
 
 void Generator::generate() {
 
---
+++ build/Tetris_by_it2000/CMakeFiles/Tetris.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/it2000/Documenti/Programmazione/Tetris_by_it2000/Tetris_by_it2000/Generator.cpp" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Generator.cpp.o" "gcc" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Generator.cpp.o.d"
+  "/home/it2000/Documenti/Programmazione/Tetris_by_it2000/Tetris_by_it2000/Ghost.cpp" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Ghost.cpp.o" "gcc" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Ghost.cpp.o.d"
+  "/home/it2000/Documenti/Programmazione/Tetris_by_it2000/Tetris_by_it2000/Main.cpp" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Main.cpp.o" "gcc" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Main.cpp.o.d"
+  "/home/it2000/Documenti/Programmazione/Tetris_by_it2000/Tetris_by_it2000/Matrix.cpp" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Matrix.cpp.o" "gcc" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Matrix.cpp.o.d"
+  "/home/it2000/Documenti/Programmazione/Tetris_by_it2000/Tetris_by_it2000/Piece.cpp" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Piece.cpp.o" "gcc" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Piece.cpp.o.d"
+  "/home/it2000/Documenti/Programmazione/Tetris_by_it2000/Tetris_by_it2000/Preview.cpp" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Preview.cpp.o" "gcc" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Preview.cpp.o.d"
+  "/home/it2000/Documenti/Programmazione/Tetris_by_it2000/Tetris_by_it2000/Status.cpp" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Status.cpp.o" "gcc" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Status.cpp.o.d"
+  "/home/it2000/Documenti/Programmazione/Tetris_by_it2000/Tetris_by_it2000/Tetris.cpp" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Tetris.cpp.o" "gcc" "Tetris_by_it2000/CMakeFiles/Tetris.dir/Tetris.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
